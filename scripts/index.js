9c1f28c1d6fadc725a1424eb67ac929a869beaea
Albert Bashirov <albash7@yandex.ru>
2024-05-08 18:15:19 +0500

    Подготовил проект к первой проверке

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,15 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template");
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 const content = document.querySelector(".content");
 const places = content.querySelector(".places");
-const resetButton = document.querySelector(".card__delete-button");
+const resetButton = cardTemplate.querySelector(".card__delete-button");
 const placesList = content.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function addCard(initialCards, onDelete) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const resetButton = cardElement.querySelector(".card__delete-button");
 
   cardElement.querySelector(".card__title").textContent = initialCards.name;
   cardElement.querySelector(".card__image").src = initialCards.link;
@@ -20,14 +21,17 @@ function addCard(initialCards, onDelete) {
 
   return cardElement;
 }
-const result = addCard(initialCards[0], deleteCard);
-console.log(result);
+
 // @todo: Функция удаления карточки
-function deleteCard() {}
+function deleteCard(resetButton) {
+  resetButton.remove();
+}
+
 // @todo: Вывести карточки на страницу
 function renderCards() {
   for (let i = 0; i < initialCards.length; i++) {
-    placesList.append(initialCards[i]);
+    const result = addCard(initialCards[i], deleteCard);
+    placesList.append(result);
   }
 }
 renderCards();
